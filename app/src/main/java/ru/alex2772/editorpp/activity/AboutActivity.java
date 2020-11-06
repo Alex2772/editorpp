f8a385ef3b3cf4162196fee869bfe37b31b71ee5
alex2772-mi <alex2772@alex2772.ru>
2020-11-06 18:37:23 +0300

    Added Github link to About window

--- app/src/main/java/ru/alex2772/editorpp/activity/AboutActivity.java
+++ app/src/main/java/ru/alex2772/editorpp/activity/AboutActivity.java
@@ -8,6 +8,8 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
 import android.widget.TextView;
 
@@ -27,6 +29,10 @@ public class AboutActivity extends AppCompatActivity {
 
         TextView version = findViewById(R.id.version);
         version.setText(getResources().getString(R.string.about_version, BuildConfig.VERSION_NAME));
+
+        TextView sourceCode = findViewById(R.id.textView10);
+        sourceCode.setText(Html.fromHtml(String.format("<a href=\"https://github.com/Alex2772/editorpp\">%s</a>", getResources().getString(R.string.source_code))));
+        sourceCode.setMovementMethod(LinkMovementMethod.getInstance());
     }
 
 }
